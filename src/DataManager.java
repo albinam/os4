eee1913dad9c8ea221b145bda3ecf6cdbfa0160a
albinosikm <albinosikm@gmail.com>
2020-05-12 15:29:20 +0400

    changes

--- src/DataManager.java
+++ src/DataManager.java
@@ -6,6 +6,7 @@ public class DataManager {
 	private int chsize = 0;
 	int fileId = -1;
 	private int freememory = Block.n;
+	private ArrayList<Knot> knots = new ArrayList<Knot>();;
 
 	public void CreateFile(int size) {
 		startindex = true;
@@ -24,14 +25,15 @@ public class DataManager {
 						i++;
 					}
 				}
-				file.addKnot(Block.memory.get(i));
+				int fileid = file.getId();
+				addKnot(Block.memory.get(i), fileid);
 				freememory--;
 				chsize++;
 			}
 			if (size == chsize) {
 				Files.add(fileId, file);
-				Main.textAreaWindow.append("Добавлен файл № " + fileId + "\n");
-				Main.textAreaWindow.append("Свободно памяти: " + freememory + "\n");
+				Main.textAreaWindow.append("Добавлен файл №" + fileId + "\n");
+				Main.textAreaWindow.append("Свободно памяти:" + freememory + "\n");
 				return;
 			}
 		}
@@ -41,9 +43,23 @@ public class DataManager {
 			Main.textAreaWindow.append("Данного файла не сущетвует! \n");
 		} else {
 			freememory = freememory + Files.get(id).fileSize();
-			Files.get(id).removeKnots();
-			Main.textAreaWindow.append("Удалён файл " + Main.textFieldId.getText() + "\n");
+			removeKnots(id);
+			Main.textAreaWindow.append("Удалён файл" + Main.textFieldId.getText() + "\n");
 		}
-		Main.textAreaWindow.append("Свободно памяти: " + freememory + "\n");
+		Main.textAreaWindow.append("Свободно памяти:" + freememory + "\n");
+
+	}
+	public void addKnot(Knot knot, int fileid) {
+		knots.add(knot);
+		knot.setFile(fileid);
+	}
+
+	public void removeKnots(int fileid) {
+		for (Knot knot : knots) {
+			if (knot.getId() == fileid) {
+				knot.setFile(-1);
+			}
+		}
+		knots.clear();
 	}
 }
--- src/File.java
+++ src/File.java
@@ -3,30 +3,17 @@ import java.util.ArrayList;
 public class File {
 	private int fileSize;
 	private int fileId;
-	private ArrayList<Knot> knots;
+	private Knot knots;
 
 	public File(int fileId, int fileSize) {
-		knots = new ArrayList<Knot>();
 		this.fileId = fileId;
 		this.fileSize = fileSize;
 	}
 
 	public Knot getKnot(int index) {
-		return knots.get(index);
+		return knots.nextKnot();
 	}
-
-	public void addKnot(Knot knot) {
-		knots.add(knot);
-		knot.setFile(fileId);
-	}
-
-	public void removeKnots() {
-		for (Knot knot : knots) {
-			knot.setFile(-1);
-		}
-		knots.clear();
-	}
-
+	
 	public int getId() {
 		return fileId;
 	}
